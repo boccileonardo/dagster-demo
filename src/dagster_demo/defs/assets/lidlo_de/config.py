3c298bcb4679724cd3e1e0f889e0127013f1d11d
leo <leonardobocci99@gmail.com>
2025-07-19 16:41:52 +0200

    use config factory

--- src/dagster_demo/defs/assets/lidlo_de/config.py
+++ src/dagster_demo/defs/assets/lidlo_de/config.py
@@ -1,16 +1,11 @@
 # EXAMPLE DATA: faker/data/single_file_many_dates
 
-from dagster_demo.components.constants import LANDING_ZONE, RETAILER_CONFIG
+from dagster_demo.components.config_utils import build_retailer_config
 
-# MANUAL INPUT
 RETAILER_ID = 1002
 DATA_SOURCE_NAME = "uploader"
 DATE_COLUMN = "date"
 
-# DO NOT EDIT MANUALLY
-DIRECTORY = f"{LANDING_ZONE}/{RETAILER_ID}/"
-# fail if no config in secure group key dict. Allow no SGK - treated as open access
-SECURE_GROUP_KEY = RETAILER_CONFIG[RETAILER_ID].get("sgk", 0)
-RETAILER_NAME = RETAILER_CONFIG[RETAILER_ID]["name"]
-REGION = RETAILER_CONFIG[RETAILER_ID]["region"]
-COUNTRY = RETAILER_CONFIG[RETAILER_ID]["country"]
+DIRECTORY, SECURE_GROUP_KEY, RETAILER_NAME, REGION, COUNTRY = build_retailer_config(
+    RETAILER_ID
+)
